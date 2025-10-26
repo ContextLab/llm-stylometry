d888219f513cad92e0884d2e2f8d2e6fa7633352
Jeremy Manning <jeremy.r.manning@dartmouth.edu>
2025-10-26 11:56:39 -0400

    Use HTML for title to keep logo and text on same line
    - Changed from markdown heading to <h1> tag
    - Image and text now inline
    - Added vertical-align: middle for better alignment
    
    Ref: #42

--- code/generate_dataset_card.py
+++ code/generate_dataset_card.py
@@ -124,7 +124,10 @@ size_categories:
 pretty_name: {metadata['full_name']} Corpus
 ---
 
-# <img src="https://cdn-avatars.huggingface.co/v1/production/uploads/1654865912089-62a33fd71424f432574c348b.png" alt="ContextLab" width="25"/> ContextLab {metadata['full_name']} Corpus
+<h1>
+  <img src="https://cdn-avatars.huggingface.co/v1/production/uploads/1654865912089-62a33fd71424f432574c348b.png" alt="ContextLab" width="25" style="vertical-align: middle;"/>
+  ContextLab {metadata['full_name']} Corpus
+</h1>
 
 ## Dataset Description
 
